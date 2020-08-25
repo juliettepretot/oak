a8092113fc0f81b389d3d4b2b5c0600e354516a4
Juliette Pretot <julsh@google.com>
2020-08-25 15:22:53 +0000

    Create proto dir if it doesn't exist

--- oak_runtime/src/introspection_browser_client/build.sh
+++ oak_runtime/src/introspection_browser_client/build.sh
@@ -4,6 +4,7 @@
 npm ci
 
 # Generate JavaScript code from the introspection_events proto
+mkdir -p proto
 protoc --proto_path=../../../proto --js_out=import_style=commonjs,binary:proto ../../../proto/introspection_events.proto
 
 # Build JavaScript bundle
